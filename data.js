89609c9fa4e351162c02ba03f5845114587c67c4
ZeckeyBay <zfatihturan@gmail.com>
2023-03-19 17:22:29 +0300

    sproduct page removed simpler design

---
+++ data.js
@@ -0,0 +1,73 @@
+let shopItemsData = [{
+    id:"11",
+    brand:"Zenith",
+    name:"Red T-Shirt",
+    price:100,
+    img:"images/products/tshirts/red-tshirt"
+},{
+    id:"12",
+    brand:"Zenith",
+    name:"Green T-Shirt",
+    price:90,
+    img:"images/products/tshirts/green-shirt"
+},{
+    id:"13",
+    brand:"Zenith",
+    name:"Blue T-Shirt",
+    price:95,
+    img:"images/products/tshirts/blue-tshirt.jpg"
+},{
+    id:"14",
+    brand:"Zenith",
+    name:"Navy-Blue T-Shirt",
+    price:80,
+    img:"images/products/tshirts/black-tshirt.jpg"
+},{
+    id:"15",
+    brand:"Zenith",
+    name:"Navy-Blue Shirt",
+    price:150,
+    img:"images/products/shirts/blue-shirt.jpg"
+},{
+    id:"16",
+    brand:"Zenith",
+    name:"Green Black Shirt",
+    price:130,
+    img:"images/products/shirts/green-shirt.jpg"
+},{
+    id:"17",
+    brand:"Zenith",
+    name:"Velvet Shirt",
+    price:160,
+    img:"images/products/shirts/longsleeved-shirt"
+},{
+    id:"18",
+    brand:"Zenith",
+    name:"Black Shirt",
+    price:120,
+    img:"images/products/shirts/navyblue-shirt"
+},{
+    id:"19",
+    brand:"Zenith",
+    name:"Blue Jeans",
+    price:200,
+    img:"images/products/trousers/blue-jeans"
+},{
+    id:"20",
+    brand:"Zenith",
+    name:"Navy-Blue Jeans",
+    price:220,
+    img:"images/products/trousers/navyblue-jeans"
+},{
+    id:"21",
+    brand:"Zenith",
+    name:"Women Blue Jeans",
+    price:200,
+    img:"images/products/trousers/w-blue-jeans"
+},{
+    id:"22",
+    brand:"Zenith",
+    name:"Women Navy-Blue Jeans",
+    price:220,
+    img:"images/products/trousers/w-navyblue-jeans"
+},]
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("navbar");
-const close = document.getElementById("close");
+let shop = document.getElementById('shop-container');
+let pdetails = document.getElementById("prodetails")
 
-if(bar){
-    bar.addEventListener("click", ()=>{
-        nav.classList.add("active");
-    })
-}
+let basket = [];
 
-if(close){
-    close.addEventListener("click", ()=>{
-        nav.classList.remove("active");
-    })
-}
+let generateShop = () =>{
+    return (shop.innerHTML= shopItemsData.map((x)=>{
+        let {id,name,price,brand,img} = x;
+        return `
+        <div id=product-id-${id} class="pro">
+            <img src=${img}>
+            <div class="des">
+                <span>${brand}</span>
+                <h5>${name}</h5>
+                <h4>$${price}</h4>
+            </div>
+            <div>
+                <h4>-<h4>
+                <a href="#" onclick="window.location.href='cart.html';"><i class="bi bi-cart2 cart"></i></a>
+            </div>
+        </div> `
+    }).join(""));
+};
+
+
+generateShop();
